1a83442669258de148504efd20f310efcf77cd7f
zain-tanveer-098 <zaintanveerking@gmail.com>
2026-02-23 13:02:04 +0500

    postgresql

--- clickup_clone/clickup_clone/settings.py
+++ clickup_clone/clickup_clone/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'clickup_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clickup_clone_db',
+        'USER': 'clickup_user',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
